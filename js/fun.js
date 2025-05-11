68eb47eb61693a46ad976b0a54d77e4e3d3605a4
VictorS-67 <victor.schneider67@gmail.com>
2025-05-11 13:05:35 +0900

    modified: survey.html

--- js/fun.js
+++ js/fun.js
@@ -22,3 +22,51 @@ function parseCSV(data) {
     }, {});
   });
 }
+
+
+function resetDisplay(filteredData) {
+  onomatopoeiaInput.value = "";
+  startDisplay.textContent = "-.--";
+  endDisplay.textContent = "-.--";
+
+  if (filteredData == []) {
+   	recordMessage = "None";
+  } else {
+    recordMessage = "";
+    filteredData.forEach(elt => {
+      recordMessage += "\n" + elt ;
+    });
+  }
+  recordOnomatopoeia.textContent = recordMessage;
+}
+
+function saveOnomatopoeia(onomatopoeia, startTime, endTime, filteredData) {
+  if (onomatopoeia === "") {
+    messageDisplay.textContent = "Please enter your onomatopoeia.";
+    messageDisplay.style.color = "red";
+    return;
+  }
+  if (startTime === "-.--") {
+    messageDisplay.textContent = "Please record the start of the onomatopoeia.";
+    messageDisplay.style.color = "red";
+    return;
+  }
+  if (endTime === "-.--") {
+    messageDisplay.textContent = "Please record the end of the onomatopoeia.";
+    messageDisplay.style.color = "red";
+    return;
+  }
+
+  // store the data in the sheet online
+  
+
+  // update the local filteredData
+
+
+  // Display a success message
+  messageDisplay.textContent = "Onomatopoeia and start-end saved!";
+  messageDisplay.style.color = "green";
+
+  resetDisplay(filteredData); // Clear display after saving
+
+}
